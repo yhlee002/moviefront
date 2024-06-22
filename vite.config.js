a4885ab18306f38598b838747002cad7958b1f4f
yhlee <xxxoxxo002@gmail.com>
2024-06-22 21:32:51 +0900

    vue proxy 서버 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '^/api': {
+        target: 'http://10.0.3.80:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [
     vue(),
     VueDevTools(),
