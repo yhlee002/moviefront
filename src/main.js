dcd3040f8df61e725bfe96ec9510fc64a0868c37
yhlee <xxxoxxo002@gmail.com>
2024-06-23 03:24:05 +0900

    클라이언트 단의 HTTP request에 Access-Control-Allow-Origin 헤더 롤백

--- src/main.js
+++ src/main.js
@@ -20,8 +20,11 @@ axios.defaults.baseURL = 'http://' + import.meta.env.VITE_APP_HOST + ':' + impor
 // }
 axios.defaults.withXSRFToken = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-// axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = `${import.meta.env.VITE_APP_HOST}:${import.meta.env.VITE_APP_POST}`;
+axios.defaults.headers.patch['Access-Control-Allow-Origin'] = `${import.meta.env.VITE_APP_HOST}:${import.meta.env.VITE_APP_POST}`;
+axios.defaults.headers.delete['Access-Control-Allow-Origin'] = `${import.meta.env.VITE_APP_HOST}:${import.meta.env.VITE_APP_POST}`;
+axios.defaults.headers.options['Access-Control-Allow-Origin'] = `${import.meta.env.VITE_APP_HOST}:${import.meta.env.VITE_APP_POST}`;
+
 axios.defaults.withCredentials = true;
 
 // mitt
