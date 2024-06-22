e4ba53c2bad1c1f27dd8dc5b62eb9ebf604e0344
yhlee <xxxoxxo002@gmail.com>
2024-06-23 01:22:09 +0900

    Revert "vue proxy 서버 설정 추가"
    This reverts commit a4885ab1

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80,
-    proxy: {
-      '/api': {
-        target: `http://localhost:8080/api`,
-        changeOrigin: true,
-      }
-    }
+    port: 80
   },
   plugins: [
     vue(),
