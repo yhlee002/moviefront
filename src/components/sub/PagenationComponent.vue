8dc73616a8567fd241d90c67304791998551bd8c
yhlee <xxxoxxo002@gmail.com>
2024-07-18 13:18:30 +0900

    -

--- src/components/sub/PagenationComponent.vue
+++ src/components/sub/PagenationComponent.vue
@@ -21,7 +21,7 @@ watch(page, (newPage) => {
     }, {immediate: true}
 )
 
-updateShowPages(props.pages, page.value);
+// updateShowPages(props.pages, page);
 
 function updateShowPages(pages, page) {
   showPages.value = [];
@@ -93,15 +93,15 @@ function goNextPage() {
     </div>
     <div>
       <ul class="pagenation-page-list">
-        <li v-for="idx in showPages" :key="`page-${idx}`" @click="getPage(idx)"
-            :class="idx === page? 'current-page' : ''">{{ idx }}
-        </li>
-        <!--        <router-link v-for="idx in showPages" :key="`page-${idx}`"-->
-        <!--                     :to="{ name: category, query: { page: idx }, force: true }">-->
-        <!--          <li :class="idx === page? 'current-page' : ''">-->
-        <!--            {{ idx }}-->
-        <!--          </li>-->
-        <!--        </router-link>-->
+<!--        <li v-for="idx in showPages" :key="`page-${idx}`" @click="getPage(idx)"-->
+<!--            :class="idx === page? 'current-page' : ''">{{ idx }}-->
+<!--        </li>-->
+        <router-link v-for="idx in showPages" :key="`page-${idx}`"
+                     :to="{ name: category, query: { page: idx }, force: true }">
+          <li :class="idx === page? 'current-page' : ''">
+            {{ idx }}
+          </li>
+        </router-link>
       </ul>
     </div>
     <div class="next-button">
